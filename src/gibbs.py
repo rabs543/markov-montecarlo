9d71955c441a3fab67ca72a9984767a0a1d4da3a
rabs543 <rburnettstuart@gmail.com>
2024-04-03 11:52:21 +0100

    Rewrote code to allow for multiple parameters in Gibbs sampler

--- src/gibbs.py
+++ src/gibbs.py
@@ -1,25 +1,46 @@
-from typing import Callable, List, get_args
+from typing import Callable, List, Union, get_args
 
 import numpy as np
 
 
 def gibbs_sampler(
-    m: int,  # The dimension of the sampler
-    x_0: np.array,  # The initial value
-    T: int,  # The time to calculate to
-    conditional: Callable[..., float],  # The conditional distributions p(x_i | x_{-i})
+    x_0: List[np.array],  # The initial value
+    n: int,  # The length of the Markov chain
+    conditional: List[
+        Callable[..., float]
+    ],  # The conditional distributions p(x_i | x_{-i})
 ):
-    if len(x_0) != m:
-        raise TypeError("x_0 should have length m")
+    if len(x_0) != len(conditional):
+        raise TypeError("There should be a conditional distribution for each x_0")
 
-    if len(get_args(conditional)) != m - 1:
-        raise TypeError("conditional should take m-1 arguments")
+    parameter_dimensions = [len(x) for x in x_0]
 
-    result = np.zeros((T + 1) * m)
-    result[:m] = x_0
-    for i in range(T * m):
-        result[m + i] = conditional(*result[i : m + i])
+    result: List[np.array] = []
+    for i, x in enumerate(x_0):
+        m = parameter_dimensions[i]
+        y = np.zeros(m * n)
+        y[:m] = x
+        result.append(y)
 
-    result.reshape(m, T)
+    for i in range(0, n - 1):
+        for j in range(len(x_0)):
+            m = parameter_dimensions[j]
+            for k in range(m):
+                result[j][(i + 1) * m + k] = conditional[j](
+                    # We always want to take the most recent len(x_0) values
+                    k,
+                    *(
+                        x[
+                            parameter_dimensions[h]
+                            * i : parameter_dimensions[h]
+                            * (i + 1)
+                        ]
+                        for h, x in enumerate(result)
+                    )
+                )
 
-    return result
+    reshaped_results = []
+    for i, parameter in enumerate(result):
+        reshaped_results.append(parameter.reshape(-1, parameter_dimensions[i]))
+
+    return reshaped_results
